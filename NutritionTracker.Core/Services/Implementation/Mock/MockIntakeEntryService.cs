54dd75793cca4223e12f8cc7348c4868d6da41a7
Jdalbano2 <jdalbano3@ivytech.edu>
2022-06-27 21:31:13 -0400

    Intake Entries and IIntakeEntries

---
+++ NutritionTracker.Core/Services/Implementation/Mock/MockIntakeEntryService.cs
@@ -0,0 +1,32 @@
+﻿using NutritionTracker.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NutritionTracker.Core.Services.Implementation.Mock
+{
+    internal class MockIntakeEntryService
+    {
+        public List<IntakeEntry> GetIntakeEntries()
+        {
+
+            return new List<IntakeEntry>
+            {
+                new IntakeEntry
+                {
+                    Calories = 1000,
+                    Fat = 518,
+                    Protein = 20,
+                    Carbs = 59,
+                },
+                new IntakeEntry
+                {
+                    Calories = 223,
+                    Fat = 23,
+                    Protein = 240,
+                    Carbs = 5229,
+                },
+            };
+        }
+    }
+}
---
+++ NutritionTracker.Core/Services/Interface/IIntakeEntryService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NutritionTracker.Core.Services.Interface
+{
+    internal interface IIntakeEntryService
+    {
+    }
+}
